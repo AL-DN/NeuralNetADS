76f7d92ef18c7962d38bd4d3705d914c54a3e444
Sahi <axs9247@miami.edu>
2024-10-06 20:52:29 -0400

    implements randomized weights for each neuron connection

--- neuralNetADS.cpp
+++ neuralNetADS.cpp
@@ -1,15 +1,48 @@
 // Author: Alden Sahi
 // Date: Oct 2024
 // Project Name: NeuralNetADS
-//Project Description: Implementing a Neural Network using CPP
+//Project Description: Implementing a Implicityly and Fully Connected Neural Network using CPP
 #include <vector>
+#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
-class Neuron;
+struct Connection 
+{
+    double weight;
+    double deltaweight;
 
+};
+
+class Neuron;
 typedef vector<Neuron> Layer;
 
+/*------------------ NEURON CLASS ----------------*/
+class Neuron {
+    public:
+    Neuron(unsigned numOutputs);
+
+    private:
+        static double randomWeight(void) { return rand()/double(RAND_MAX)}
+        double m_ouputVal;
+        // pf type Connection since we need to store weight and the change in weight
+        vector<Connection> m_outputWeights;
+
+};
+
+/*-------------- External Neuron Constructor Definition-----------------*/
+
+
+Neuron::Neuron(unsigned numOutputs) {
+    for (unsigned i = 0; i < numOutputs; ++i ) {
+        m_outputWeights.push_back(Connection());
+        m_outputWeights.back().weight= randomWeight();
+    }
+};
+
+
+/*-------------------Network Class------------*/
 class Network {
     public:
         //constructor
@@ -17,19 +50,23 @@ class Network {
         void feedFoward(const vector<double> &inputVals);
         void backProp(const vector<double> &targetVals);
         void getResults(vector<double> &resultsVals) const;
+
     private:
         // m_layers[layerNum][NeuronNum]
         vector<Layer> m_layers;
 };
 
+/*-------------- External Network Constructor Definition-----------------*/
+
 Network:: Network( const vector<unsigned> &topology) {
     unsigned numLayers = topology.size();
-    unsigned numNeurons = topology.size();
-
+    
     for (unsigned layerNum = 0 ; layerNum < numLayers; ++layerNum) {
         m_layers.push_back(Layer());
+        unsigned numOutputs = layerNum == topology.size() - 1 ? 0 : topology[layerNum + 1];
+
         for(unsigned neuronNum = 0; neuronNum <= topology[layerNum]; ++neuronNum ) {
-            m_layers.back().push_back(Neuron());
+            m_layers.back().push_back(Neuron(numOutputs));
         }
     }
 }
